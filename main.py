796492a7cc211f121a50c05c36959269490735cc
lewiatan <lewicki.pk@gmail.com>
2016-09-28 00:46:05 +0200

    playing around

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python3
 import sys
 
-plik = open(sys.argv[0], "r")
+# get the line number with the game no
+gameNoString = "#Game No :"
 
-for line in plik:
-    line = line.rstrip()
-    print(line)
+#plik = open(sys.argv[1], "r")
+#plik = open("./888poker.txt", "r")
+#
+#for line in plik:
+#    line = line.rstrip()
+#    if gameNoString in line:
+#        print(lineself.)
+
+lookup = gameNoString
+
+#with open(sys.argv[1]) as myFile:
+numeryGier = dict()
+counter = 0
+with open("./888poker.txt") as myFile:
+    for num, line in enumerate(myFile, 1):
+        if lookup in line:
+            #print( 'found at line:', num)
+            numeryGier[counter] = line
+            counter += 1
+
+print(numeryGier)
